6e402707baf14301c66aefd14ba5a305fdd3aa45
Ishant Dahiya <ishantdahiya@gmail.com>
2024-08-22 23:22:34 +0000

    setup alembic and model migrations

---
+++ app/api/v1/auth/models.py
@@ -0,0 +1,38 @@
+import bcrypt
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+
+from app.database import Base
+
+
+class User(Base):
+    """User database model."""
+
+    __tablename__ = "users"
+    __table_args__ = ()
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, nullable=False, index=True, unique=True)
+    password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+
+    def __init__(
+        self,
+        email: str,
+        password: str,
+        name: str,
+    ):
+        self.email = email
+        if password:
+            self.password = generate_password_hash(password)
+        self.name = name.title()
+
+    def __repr__(self) -> str:
+        return f"User {self.id}: {self.email}"
+
+
+def generate_password_hash(password: str) -> str:
+    """Bcrypts a password, returns a hash string"""
+    pwd_bytes = password.encode("ascii")
+    salt_bytes = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt_bytes).decode("ascii")
